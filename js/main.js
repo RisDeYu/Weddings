d00b8590dcb59bbcb3af1c33082a40ca1fd4a3b3
ningsihapriyani <110323467+ningsihapriyani@users.noreply.github.com>
2023-06-11 03:24:45 +0700

    benerin semua
    kurang gallery ama chatbox

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ let tl = gsap.timeline(
 
 tl.from('.wrap-arrum',{delay:.5, duration:1, y:100, opacity:0});
 tl.from('.jdl-buka',{delay:.6, duration:1, opacity:0});
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
