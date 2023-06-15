7db06f49010b4349d748d9e0792c10a441ae229b
ningsihapriyani <92260126+RisDeYu@users.noreply.github.com>
2023-06-15 12:15:58 +0700

    v4
    semua kelar gatau ada refisi apa ngga

--- js/script.js
+++ js/script.js
@@ -15,3 +15,47 @@ function myFunction1() {
   // Alert the copied text
   //alert("Ingin menyalin nomor rekening?");
 }
+
+const PlayPause = document.getElementById('play-pause');
+const Mute = document.getElementById('mute');
+const Audio = document.querySelector('audio ');
+
+let song_Playing = true;
+
+function putarAudio(){
+  song_Playing = true;
+  Audio.play();
+  PlayPause.classList.add('active');
+
+  // nampilin ikon
+  PlayPause.innerHTML = '<ion-icon name="pause-circle-outline"></ion-icon>';
+}
+
+function stopAudio(){
+  song_Playing = false;
+  Audio.pause();
+  PlayPause.classList.remove('active');
+
+  //gaanti ikonnya
+  PlayPause.innerHTML = '<ion-icon name="play-circle-outline"></ion-icon>';
+}
+
+//tombol play pause
+PlayPause.addEventListener("click",() => (song_Playing ? stopAudio() : putarAudio()));
+
+
+//tombol mute
+Mute.addEventListener("click",fMute);
+
+function fMute(){
+  if(Audio.muted){
+    Audio.muted = false;
+    //gaanti ikonnya
+    Mute.innerHTML = '<ion-icon name="volume-high-outline"></ion-icon>';
+  }
+  else{
+    Audio.muted = true;
+    // nampilin ikon
+    Mute.innerHTML = '<ion-icon name="volume-mute-outline"></ion-icon>';
+  }
+}
