d6253abf35744d69d0e25ee8482cda271cabfc55
ningsihapriyani <92260126+RisDeYu@users.noreply.github.com>
2023-06-18 18:01:52 +0700

    v4
    nambah nav bar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,51 @@
+// Get the button
+let mybutton = document.querySelector("header");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Set the date we're counting down to
+var countDownDate = new Date("July 08, 2023 10:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  //document.getElementById("hitung-mundur").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  document.getElementById("days").innerText = days,
+  document.getElementById("hours").innerText = hours,
+  document.getElementById("minutes").innerText = minutes,
+  document.getElementById("seconds").innerText = seconds;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Akad Nikah Telah Dilaksanakan";
+  }
+}, 1000);
+
+
 function myFunction() {
 
   // Copy the text inside the text field
